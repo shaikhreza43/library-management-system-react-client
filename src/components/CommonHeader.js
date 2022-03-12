a4ab7b71e7436f2080aee6cbbc8fd93fae745599
shaikhreza43 <reza.rk786@gmail.com>
2022-03-13 00:44:04 +0530

    Fixed Bug related to Navbar Alignment

--- src/components/CommonHeader.js
+++ src/components/CommonHeader.js
@@ -7,6 +7,10 @@ import {
   Nav,
   NavItem,
   NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
 } from "reactstrap";
 
 export default function CommonHeader(props) {
@@ -27,15 +31,31 @@ export default function CommonHeader(props) {
               <NavLink href="/home">Home</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/about">
-                About
-              </NavLink>
+              <NavLink href="/about">About</NavLink>
             </NavItem>
-            <NavItem style={{position:'fixed',right:'30px'}}>
-              <NavLink href="/auth">
-                Login
-              </NavLink>
+          </Nav>
+          <Nav
+            className="mr-auto justify-content-end"
+            style={{ width: "100%" }}
+            navbar
+          >
+            <NavItem>
+              <NavLink href="/auth">Login</NavLink>
             </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Welcome User
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>My Account</DropdownItem>
+                <DropdownItem>Request Book</DropdownItem>
+                <DropdownItem>Add Student</DropdownItem>
+                <DropdownItem>Add Librarian</DropdownItem>
+                <DropdownItem>Add Books</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Logout</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
           </Nav>
         </Collapse>
       </Navbar>
