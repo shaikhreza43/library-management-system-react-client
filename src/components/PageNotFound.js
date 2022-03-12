17b6f61cdaebb1f8c83ed93facdf4468ac6161d2
shaikhreza43 <reza.rk786@gmail.com>
2022-03-12 21:29:29 +0530

    Page Not Found Implemented

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import Link from "@mui/material/Link";
 
 export default function PageNotFound(props) {
   return (
-    <div>PageNotFound</div>
-  )
+    <div id="error-page">
+      <div className="content">
+        <h2 className="header" data-text="404">
+          404
+        </h2>
+        <h4 data-text="Opps! Page not found">Opps! Page not found</h4>
+        <p>Sorry, the page you're looking for doesn't exist.</p>
+        <div className="btns">
+          <Link href="/">return home</Link>
+          <Link
+            href=""
+            onClick={(e) => {
+              e.preventDefault();
+              window.history.back();
+            }}
+          >
+            go back
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
